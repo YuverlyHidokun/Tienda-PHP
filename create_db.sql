321220e095f144babb02d5c7985c69e9bd3c73ed
YuverlyHidokun <yuverlyverdezoto@gmail.com>
2025-07-24 14:33:06 -0500

    imagenes iniciales

--- create_db.sql
+++ create_db.sql
@@ -25,12 +25,32 @@ CREATE TABLE usuarios (
     contrasena VARCHAR(255) NOT NULL
 );
 
+--Tabla de mensajes
+CREATE TABLE mensajes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100),
+    correo VARCHAR(100),
+    mensaje TEXT,
+    fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 -- Poblado inciial de categorías
 INSERT INTO categoria (nombre) VALUES
 ('Tecnología'),
 ('Moda'),
 ('Hogar'),
 ('Libros');
+
+--poblado inicial de porductos
+INSERT INTO productos (nombre, precio, foto, categoria_id) VALUES
+('Laptop HP 15"', 799.99, 'laptop-hp.jpg', 1),
+('Smartphone Galaxy A15', 299.99, 'galaxy-a15.jpg', 1),
+('Vestido de verano', 45.00, 'vestido-verano.jpg', 2),
+('Sofá 3 puestos', 399.99, 'sofa.jpg', 3),
+('Harry Potter y la Piedra Filosofal', 20.50, 'harry-potter.jpg', 4),
+('Audífonos Bluetooth', 25.00, 'audifonos.jpg', 1),
+('Chaqueta de cuero', 120.00, 'chaqueta-cuero.jpg', 2);
+
 
 -- Contraseña: 123456 (usaremos https://onlinephp.io/password-hash)
 INSERT INTO usuarios (correo, contrasena)
